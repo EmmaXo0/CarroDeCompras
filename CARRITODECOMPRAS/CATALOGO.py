921c84217a50374e25c8c270c428d58fabbf4668
Luis Antonio <im221434@itsatlixco.edu.mx>
2026-09-17 02:32:09 -0600

    SE AMPLIO LA VARIEDAD DEL CATALOGO

--- CARRITODECOMPRAS/CATALOGO.py
+++ CARRITODECOMPRAS/CATALOGO.py
@@ -9,7 +9,15 @@ def cargar_catalogo():
     catalogo = {
         "P001": {"nombre": "Café", "precio": 45.0, "stock": 20},
         "P002": {"nombre": "Té", "precio": 30.0, "stock": 15},
-        "P003": {"nombre": "Galleta", "precio": 15.0, "stock": 50}
+        "P003": {"nombre": "Galleta", "precio": 15.0, "stock": 50},
+        "P004": {"nombre": "Leche", "precio":18.0, "stock": 35 },
+        "P005": {"nombre": "Cereal", "precio":35.0, "stock": 40 },
+        "P006": {"nombre": "Doritos", "precio":20.0, "stock": 25 },
+        "P007": {"nombre": "Agua", "precio":15.0, "stock": 50 },
+        "P008": {"nombre": "Dulces", "precio":12.0, "stock": 70 },
+        "P009": {"nombre": "Refresco", "precio":20.0, "stock": 15 },
+        "P010": {"nombre": "Salsa", "precio":14.0, "stock": 25 }
+        
     }
     return catalogo
 
